5e1b0e2a5c98eb3101dc144c77a4f2257a01a4f2
Altynbek <altynbek2003@gmail.com>
2018-06-12 22:39:32 +0600

    OK

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,23 +3,20 @@ import java.util.Collections;
 import java.util.Objects;
 
 public class ArrayStorage {
-    private int N = 10000;
-    private Resume[] storage = new Resume[N];
+    private int StorageLength = 10000;
+    private Resume[] storage = new Resume[StorageLength];//Исправлено
     private int size = 0;
-
-    public ArrayStorage() {
-        size = 0;
-    }
-
-    void clear() {
+//Исправлено
+   // public ArrayStorage() {
+      //  size = 0;
+        void clear() {
         for (int i = 0; i < size; i++) {
             storage[i] = null;
         }
         size = 0;
     }
-
     void save(Resume r) {
-        if (size < N) {
+        if (size < StorageLength) {
             storage[size] = r;
             size++;
         } else {
@@ -27,11 +24,9 @@ public class ArrayStorage {
         }
     }
 
-//int getIndex(String uuid){ }
-
     Resume get(String uuid) {
-        final String must_not_be_null = Objects.requireNonNull(uuid, "uuid не должен быть null");
-        for (int i = 0; i < size; i = i + 1) {
+        Objects.requireNonNull(uuid, "uuis не должен быть равен null");
+        for (int i = 0; i < size; i ++) {
             Resume resume = storage[i];
             if (resume != null && uuid.equals(resume.uuid)) {
                 return resume;
@@ -42,7 +37,6 @@ public class ArrayStorage {
         }
         return null;
     }
-
     void delete(String uuid) {
         for (int i = 0; i < size; i++) {
             if (storage[i].uuid.equals(uuid)) {
@@ -50,16 +44,11 @@ public class ArrayStorage {
                 storage[size - 1] = null;
                 size--;
                 return;
-
             }
         }
         System.out.println("Резюме не существует");
     }
-
-
     Resume[] getAll() {
         return Arrays.copyOfRange(storage, 0, size);
-
-
     }
 }
