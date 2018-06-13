cefe7907f3d954618b64681b1a6099e47d17485c
Altynbek <altynbek2003@gmail.com>
2018-06-13 14:14:46 +0600

    OK

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,17 +1,17 @@
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.Objects;
 
 public class ArrayStorage {
     private int StorageLength = 10000;
-    private Resume[] storage = new Resume[StorageLength];//Исправлено
+    private Resume[] storage = new Resume[StorageLength];
     private int size = 0;
-        void clear() {
+
+    void clear() {
         for (int i = 0; i < size; i++) {
             storage[i] = null;
         }
         size = 0;
     }
+
     void save(Resume r) {
         if (size < StorageLength) {
             storage[size] = r;
@@ -21,24 +21,40 @@ public class ArrayStorage {
         }
     }
 
-    Resume get(String uuid) {
+    Resume[] get(String uuid) {
         Objects.requireNonNull(uuid, "uuis не должен быть равен null");
-        for (int i = 0; i < size; i ++) {
-            Resume resume = storage[i];
-            return null;
-    }
-    void delete(String uuid) {
         for (int i = 0; i < size; i++) {
-            if (storage[i].uuid.equals(uuid)) {
-                storage[i] = storage[size - 1];
-                storage[size - 1] = null;
-                size--;
-                return;
+            Resume resume = storage[i];
+            return new Resume[]{storage[i]};
+        }
+
+        void delete (String uuid)
+        {
+            for (int i = 0; i < size; i++) {
+                if (storage[i].uuid.equals(uuid)) {
+                    storage[i] = storage[size - 1];
+                    storage[size - 1] = null;
+                    size--;
+                }
+            }
+            System.out.println("Резюме не существует");
+        }
+
+        Resume[] getAll;
+        {
+            Resume[] NonEmpty = new Resume[size];
+            System.arraycopy(storage, 0, NonEmpty, 0, size);
+            return NonEmpty;
+        }
+    }
+
+    int size(String uuid) {
+
+        for (int i = 0; i < size; i++) {
+            if (uuid.equals(storage[i])) {
+                return i;
             }
         }
-        System.out.println("Резюме не существует");
-    }
-    Resume[] getAll() {
-        return Arrays.copyOfRange(storage, 0, size);
+        return 0;
     }
 }
