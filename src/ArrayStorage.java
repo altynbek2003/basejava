5b8fd26768cf845ce08f604775eead59acc9d7c6
Altynbek <altynbek2003@gmail.com>
2018-06-25 16:20:40 +0600

    OK

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,12 +1,9 @@
 /**
  * Array based storage for Resumes
  */
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Objects;
 public class ArrayStorage {
-    private int storageLenght = 10000;
-    private Resume[] storage = new Resume[storageLenght];
+    private int storageSize = 10000;
+    private Resume[] storage = new Resume[storageSize];
     private int size = 0;
 
     void clear() {
@@ -20,7 +17,7 @@ public class ArrayStorage {
     void save(Resume r)
     {
         //TODO check if resume present & check storage overflow
-        if (size < storageLenght) {storage[size] = r;
+        if (size < storageSize) {storage[size] = r;
         size++;}
     else System.out.println ("Хранилище Переполнено");
     }
@@ -36,7 +33,7 @@ public class ArrayStorage {
 
     }
 
-    public void delete(String uuid) {
+     void delete(String uuid) {
         // TODO check if resume present
         for (int i = 0; i < size; i++) {
             if (uuid==storage[i].getUuid()) {
@@ -54,15 +51,13 @@ public class ArrayStorage {
      */
 
     Resume[] getAll() {
-        Resume[] limitStroge = new Resume[size];
-        for (int i = 0; i < size; i++) {
-            limitStroge[i] = storage[i];
-        }
-        return limitStroge;
+        Resume[] limitStorage = new Resume[size];
+        System.arraycopy (storage, 0, limitStorage, 0, size);
+        return limitStorage;
 
     }
 
-    public int size() {
+    int size() {
         return size ;
     }
 
