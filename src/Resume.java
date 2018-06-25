fe9e268d5929f957cf5589580beaf3946cc72223
Altynbek <altynbek2003@gmail.com>
2018-06-25 15:50:37 +0600

    OK

--- src/Resume.java
+++ src/Resume.java
@@ -11,6 +11,14 @@ public class Resume {
         return uuid;
     }
 
-    public String Uuid() {
+    public void Uuid() {
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
     }
 }
