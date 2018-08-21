e1b43d77d99d4748d9419e57bed6942ce45ba77e
Altynbek <altynbek2003@gmail.com>
2018-08-21 21:22:00 +0600

    Удалил пусто конструктор в классе Resume и пробелы лишние

--- src/SortedArrayStorage.java
+++ src/SortedArrayStorage.java
@@ -5,21 +5,21 @@ public class SortedArrayStorage extends AbstractArrayStorage {
     protected void fillElement(int index) {
         int fillNum = size - index - 1;
         if (fillNum > 0) {
-            System.arraycopy(storage, index + 1, storage, index, fillNum);
+            System.arraycopy (storage, index + 1, storage, index, fillNum);
         }
     }
 
     @Override
     protected void addElement(Resume resume, int index) {
         int addNum = -index - 1;
-        System.arraycopy(storage, addNum, storage, addNum + 1, size - addNum);
-        storage[addNum]=resume;
+        System.arraycopy (storage, addNum, storage, addNum + 1, size - addNum);
+        storage[addNum] = resume;
     }
 
     @Override
     protected int getIndex(String uuid) {
-        Resume searchKey=new Resume();
-        searchKey.setUuid(uuid);
+        Resume searchKey = new Resume (uuid);
+        searchKey.setUuid (uuid);
         return Arrays.binarySearch (storage, 0, size, searchKey);
     }
 }
